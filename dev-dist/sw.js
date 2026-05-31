8abeeea9dd2580a69eb61b6e1c7db9aff84a5514
nsaichandana <nsaichandana2006@gmail.com>
2026-05-31 23:55:12 +0530

    Update dependencies and service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-d488705a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.421ihnog57g"
+    "revision": "0.iliteji1pqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
